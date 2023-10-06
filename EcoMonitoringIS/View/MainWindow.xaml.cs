bb68249ebeeb58987f1a41230680af267cce0901
DimaLemeshuk <dmutrolemeshuk@gmail.com>
2023-10-06 22:19:37 +0300

    Add project files.

---
+++ EcoMonitoringIS/View/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace EcoMonitoringIS.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromSeconds(1); // Оновлювати кожну секунду
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Оновлення даних в текстових полях
+            TimeTextBlock.Text = DateTime.Now.ToString("HH:mm");
+            DateTextBlock.Text = DateTime.Now.ToString("dd.MM.yyyy");
+        }
+
+        private void Page1Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new Page1();
+        }
+
+        private void Page2Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new Page2();
+        }
+
+        private void Page3Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
